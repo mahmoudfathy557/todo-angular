59e733d2738f4558f39ec98ebdba46391e8f5d18
lshorbagy <mahmoudfathy@557.com>
2023-04-04 02:52:40 +0200

    removing unwanted files

--- backend/src/core/dtos/index.ts
+++ backend/src/core/dtos/index.ts
@@ -1,5 +1,3 @@
-export * from './book.dto';
-export * from './department.gql.dto';
+ export * from './department.gql.dto';
 export * from './department.rest.dto';
-export * from './genre.dto';
-export * from './create-book-response.dto';
+ 
